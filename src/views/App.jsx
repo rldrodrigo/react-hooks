88452f269caa3c3a429e9d2afe82c23e4416123d
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-01-18 21:48:55 -0300

    useContext

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,20 +1,29 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import DataContext, { data } from '../data/DataContext'
+import { useState } from 'react/cjs/react.development'
+import Store from '../data/Store'
 
 const App = props => {
+    const [state, setState] = useState(data)
 
     return (
+        <Store>
+            <DataContext.Provider value={{ state, setState }}>
                 <div className="App">
                     <Router>
                         <Menu />
                         <Content />
                     </Router>
                 </div>
+            </DataContext.Provider>
+        </Store>
     )
+
 }
 
 export default App
